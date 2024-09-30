54f7180f0d1cea54762146e5040acfbd5811664b
SelmanKAYABAS48 <selmankayabas48@gmail.com>
2024-09-30 11:31:56 +0300

    up

--- product-list/src/components/products/ProductList.jsx
+++ product-list/src/components/products/ProductList.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+// import "./Products.scss"
 
-const ProductList = () => {
+// const ProductList = ({products}) => {
+//   console.log(products);
+//   return (
+//     <div className="products-list">
+//       {
+//         products.map((product)=>(
+
+  //! Burada tek bir ifade olduğu için parantez ile yaptık.yani tek bir kapsayıcı div var.
+          
+//           <div>
+//             <div>
+//               <h3>{product.price}</h3>
+//             </div>
+//             <img src={product.image} alt="" />
+//           </div>
+//         ))
+//       }
+//       </div>
+//   )
+// }
+
+// export default ProductList
+
+
+
+ import "./Products.scss"
+const ProductList = ({products}) => {
   return (
-    <div>ProductList</div>
+    <div className="products-list">
+      {
+        products.map((product)=>{
+          return(
+            <div key={product.id} >
+              <div><h3>{product.price}</h3></div>
+              <img src={product.image} alt={product.title} />
+            </div>
+          )
+        })
+      }
+    </div>
   )
 }
 
