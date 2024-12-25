0e9c26ab6cfff1b4557f5aae8c2258f5f24d0dd8
SelmanKAYABAS48 <selmankayabas48@gmail.com>
2024-12-25 20:15:32 +0300

    up

--- router-intro-starter/src/pages/PersonDetails.jsx
+++ router-intro-starter/src/pages/PersonDetails.jsx
@@ -4,27 +4,28 @@ import { useNavigate, useParams } from "react-router-dom";
 const PersonDetails = () => {
   const [person, setPerson] = useState([]);
 
-  const navigate = useNavigate;
+  const navigate = useNavigate();
   const { id } = useParams();
 
-  const getperson = async () => {
+  const getPerson = () => {
     fetch(`https://reqres.in/api/users/${id}`)
       .then((res) => res.json())
       .then((data) => setPerson(data.data))
       .catch((err) => console.log(err));
   };
-
   useEffect(() => {
-    getperson();
+    getPerson();
   }, []);
   return (
     <div className="container text-center mt-4">
-      <img src={person.avatar} alt={person.first_name} />
+      <img className="rounded" src={person?.avatar} alt="img" />
       <h6>
-        {person.first_name}
-        {person.last_name}
+        {" "}
+        {person?.first_name} {person?.last_name}{" "}
       </h6>
-      <p>{person.email}</p>
+      <p>{person?.email}</p>
+      <button className="btn btn-danger" onClick={() => navigate(-1)} >Back</button> <br/>
+      <button className="btn btn-success" onClick={() => navigate("/")} >Home</button>
     </div>
   );
 };
